898e546858a0ca8b4448fdeede70f9b9a2729141
Gustavo Mazurkiewitz <87877997+GustavoMaz@users.noreply.github.com>
2026-02-09 23:29:07 +0000

    Request por id de personagem

--- index.js
+++ index.js
@@ -10,10 +10,29 @@ app.get('/oi', (req, res) => {
   res.send('Olá, Mundo!')
 })
 
-const lista = ['Rick Sanchez', 'Morty Smith', 'Beth Sanchez']
+const lista = [
+  {
+    name: 'Rick Sanchez',
+    gender: 'male'
+  }, 
+  {
+    name: 'Morty Smith',
+    gender: 'male'
+  }, 
+  {
+    name: 'Beth Sanchez',
+    gender: 'female'
+  }
+]
 
 app.get('/personagens', (req, res) => {
   res.send(lista)
+})
+
+app.get('/personagens/:id', (req, res) => {
+  const id = req.params.id; //parâmetros vem com a requisição
+  const personagem = lista[id-1]
+  res.send(personagem)
 })
 
 app.listen(3000, () => {
